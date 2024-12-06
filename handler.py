146a7a5a778d1fff26fc8f7fa90ebbaf604e2729
Yeji Kim <128204727+slime4209@users.noreply.github.com>
2024-12-05 23:57:41 -0500

    Update handler.py
    updated to work with docker

--- handler.py
+++ handler.py
@@ -7,9 +7,6 @@ import requests
 import random
 import json
 
-import psycopg2
-from datetime import datetime
-
 app = Flask(__name__)
 
 API_ROOT = 'https://api.groupme.com/v3/'
@@ -38,84 +35,58 @@ FLAGGED_PHRASES = (
     'hentai',
 )
 
-# this is for logging purpose; I want to collect the scam messages to see any patterns
-# creates PostgreSQL to store messages
-def log_deleted_message_to_db(message):
-    conn = psycopg2.connect(
-        dbname=os.getenv('DB_NAME'),
-        user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASSWORD'),
-        host=os.getenv('DB_HOST')
-    )
-    cur = conn.cursor()
+# Edit these variables to customize your groupme auth token and bot id
+token = '<your groupme auth token>'
+bot_id = '<your groupme bot id>'
 
-    # Insert the deleted message into the table
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    cur.execute("INSERT INTO deleted_messages (timestamp, user_name, message_text) VALUES (%s, %s, %s)",
-                (timestamp, message['name'], message['text']))
+def get_memberships(group_id):
+    response = requests.get(f'{API_ROOT}groups/{group_id}?token={token}')
+    responses = response.json()
+    return responses
 
-    conn.commit()
-    cur.close()
-    conn.close()
-
-
-def get_memberships(group_id, token):
-    response = requests.get(f'{API_ROOT}groups/{group_id}', params={'token': token}).json()['response']['members']
-    return response
-
-def get_membership_id(group_id, user_id, token):
-    memberships = get_memberships(group_id, token)
+def get_membership_id(group_id, user_id):
+    memberships = get_memberships(group_id)
+    memberships = memberships['response']
+    memberships = memberships['members']
+    print(memberships)
     for membership in memberships:
         if membership['user_id'] == user_id:
             return membership['id']
     return None
 
-def remove_member(group_id, membership_id, token):
-    response = requests.post(f'{API_ROOT}groups/{group_id}/members/{membership_id}/remove', params={'token': token})
+def remove_member(group_id, membership_id):
+    response = requests.post(f'{API_ROOT}groups/{group_id}/members/{membership_id}/remove?token={token}') #, params={'token': token})
     print('Attempted to kick user, got response:')
     print(response.text)
     return response.ok  # Return whether the request was successful
 
 
-def delete_message(group_id, message_id, token):
+def delete_message(group_id, message_id):
     response = requests.delete(f'{API_ROOT}conversations/{group_id}/messages/{message_id}', params={'token': token})
     return response.ok
 
 
-def kick_user(group_id, user_id, token):
-    membership_id = get_membership_id(group_id, user_id, token)
+def kick_user(group_id, user_id):
+    membership_id = get_membership_id(group_id, user_id)
     if membership_id:
-        return remove_member(group_id, membership_id, token)
+        return remove_member(group_id, membership_id)
     return False
 
-@app.route('/', methods=['POST'])  # Ensure this route handles POST requests
-def webhook():
-    if request.is_json:  # Check if the incoming request is JSON
-        event = request.get_json()
-        return receive(event)
-    else:
-        return jsonify({'error': 'Request must be JSON'}), 400
-
-    
-
-
-def receive(event, context):
-    message = event
-    bot_id = message['bot_id']
-
+def receive(event):
+    message = event #json.loads(event['text'])
     for phrase in FLAGGED_PHRASES:
         if phrase in message['text'].lower():
-            # Attempt to kick the user and check if it was successful
-            if kick_user(message['group_id'], message['user_id'], message['token']):
-                delete_message(message['group_id'], message['id'], message['token'])
-                log_deleted_message_to_db(message)
+            if kick_user(message['group_id'], message['user_id']):
+                delete_message(message['group_id'], message['id'])
                 send('Kicked ' + message['name'] + ' due to apparent spam post.', bot_id)
             else:
                 print('Kick attempt failed or user is an admin.')
             break
+            print(group)
 
     return {
-        jsonify({'statusCode': 200, 'body': 'ok'})
+        'statusCode': 200,
+        'body': 'ok'
     }
 
 
@@ -127,6 +98,15 @@ def send(text, bot_id):
         'text': text,
     }
     r = requests.post(url, json=message)
+
+
+@app.route('/', methods=['POST'])  # Ensure this route handles POST requests
+def webhook():
+    if request.is_json:  # Check if the incoming request is JSON
+        event = request.get_json()
+        return receive(event)
+    else:
+        return jsonify({'error': 'Request must be JSON'}), 400
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
